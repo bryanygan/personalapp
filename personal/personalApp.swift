d9e1e28e9cec0350fde8604866d7ffdd44144775
bryangan <byg23@drexel.edu>
2025-05-12 01:04:09 -0400

    Initial Commit

---
+++ personal/personalApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalApp.swift
+//  personal
+//
+//  Created by Bryan Gan on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct personalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ personalTests/personalTests.swift
@@ -0,0 +1,17 @@
+//
+//  personalTests.swift
+//  personalTests
+//
+//  Created by Bryan Gan on 5/12/25.
+//
+
+import Testing
+@testable import personal
+
+struct personalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
